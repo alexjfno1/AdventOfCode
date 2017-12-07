9e542c10b7a6b8ab9633a02c950e266ed135c548
Alex Foster <alexjfno1@gmail.com>
2017-12-07 18:28:00 +0000

    Day6

---
+++ Day6/partTwo.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+const input = [4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3];
+// const input = [2, 4, 1, 2];
+
+let previousIterations = [];
+
+const findLargestIndex = arr => {
+  return arr.reduce((largestIndex, item, index) => {
+    if (index > 0) {
+      if ((item > arr[index - 1]) && (item > arr[largestIndex])) {
+        return index;
+      }
+    }
+
+    return largestIndex;
+  }, 0);
+}
+
+const addToIterations = arr => {
+  previousIterations.push(arr.join());
+}
+
+const hasAlreadyBeenSeen = arr => {
+  return previousIterations.includes(arr.join());
+};
+
+let iterationCount = 0;
+
+const reallocate = arr => {
+  const largestIndex = findLargestIndex(arr);
+  const numberAtIndex = arr[largestIndex];
+
+  arr[largestIndex] = 0;
+
+  let currentIndex = largestIndex + 1;
+  for (let i = 0; i < numberAtIndex; i++) {
+    if (arr[currentIndex] === undefined) {
+      currentIndex = 0;
+    }
+
+    arr[currentIndex] += 1;
+
+    currentIndex += 1;
+  }
+
+
+  if (hasAlreadyBeenSeen(arr)) {
+    const seenAtIndex = previousIterations.indexOf(arr.join());
+    console.log(iterationCount - seenAtIndex)
+  } else {
+    addToIterations(arr);
+    iterationCount += 1;
+    reallocate(arr);
+  }
+}
+
+reallocate(input);
